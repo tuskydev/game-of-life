d42756c4942bc4346e04c5fdf5af84b01c1bfb17
Diddleslip <diddleslip@gmail.com>
2020-06-23 13:45:46 -0400

    Simulation working, added rules box

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
-import anon from "./anon.jpg"
+import produce from "immer";
 
 const numRows = 20;
 const numCols = 35;
+
+const operations = [
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [-1, 0],
+  [1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  
+]
 
 function App() {
   const [grid, setGrid] = useState(() => {
@@ -15,13 +27,56 @@ function App() {
     return rows
   });
 
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      })
+    })
+    
+    setTimeout(runSimulation, 100);
+    }, []);
+
+    
   return (
     <div className="container">
       <div className="heroText">
         <h1 className="mainHeader">Welcome to the Game of Life.</h1>
 
         <div className="buttonsDiv">
-          <button>Start</button>
+          <button onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}>{running ? "Stop" : "Start"}</button>
           <button>Clear</button>
           <button>Randomize</button>
         </div>
@@ -35,12 +90,24 @@ function App() {
               <div 
               className="Grid"
               key = {`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                })
+                setGrid(newGrid);
+              }}
               style={{
                 backgroundColor: grid[i][k] ? "goldenrod" : "lightgray"}}>
               </div>
             ))
           )}
                 
+        </div>
+        <div className="rulesBox">
+          <h2 className="rules">Rules:</h2>
+          <h3><span>1. </span>Any live cell with two or three live neighbours survives.</h3>
+          <h3><span>2. </span>Any dead cell with three live neighbours becomes a live cell.</h3>
+          <h3><span>3. </span>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</h3>
         </div>
       </div>
     </div>
